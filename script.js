1e4b04bab9f9ae4581bd8f97aa8a92c2854c8086
heismauri <47569205+heismauri@users.noreply.github.com>
2020-08-17 16:48:06 -0400

    Connect to Twitter API through the same script
    Rewritten and fixed some minor mistakes

--- script.js
+++ script.js
@@ -1,71 +1,103 @@
-var tweet = {heregoestheresponseoftwitterapi}
-function getmedialinks(tweet) {
-    // Tweet is either private or not available
-    if ("errors" in tweet) {
-        var error_id = [{
-            error: tweet.errors[0].message,
-        },];
-        return error_id;
-    // Tweet has no media attached to it
-    } else if (!("extended_entities" in tweet)) {
-        var error_media = [{
-            error: "Media not found for inputted URL.",
-        },];
-        return error_media;
+// Connecting to Twitter API 1.1
+addEventListener('fetch', event => {
+    event.respondWith(handleMedia(event.request));
+});
+const twitterMedia = async request => {
+    const ourl = new URL(request.url);
+    const id = ourl.searchParams.get('id');
+    const url = 'https://api.twitter.com/1.1/statuses/show.json?tweet_mode=extended&id=' + id;
+    const resp = await fetch(url, {
+        headers: {
+            Authorization: ('Bearer ' + 'TOKEN')
+        }
+    });
+    // Success!
+    const picker_enable = ourl.searchParams.get('picker');
+    const tweet = await resp.json();
+    // Getting the media from the available tweet
+    function getmedialinks(tweet) {
+        // Private or wrong ID
+        if ('errors' in tweet) {
+            const message = tweet.errors[0].message.replace('.', '');
+            const error_message = {
+                error: message,
+            };
+            return error_message;
+        // Tweet does not have any media
+        } else if (!('extended_entities' in tweet)) {
+            const error_media = {
+                error: 'Media not found for inputted URL',
+            };
+            return error_media;
+        }
+        // Success!
+        return tweet.extended_entities.media
+            .map(media => {
+                const media_type = media.type;
+                // Video
+                if (media_type === 'video') {
+                    const video = media.video_info.variants.filter(variant => variant.bitrate !== undefined);
+                    // Video quality selector enabled
+                    if (picker_enable === 'true') {
+                        video.sort(function (a, b) {
+                            return a.bitrate - b.bitrate;
+                        });
+                        const return_video_quality = {
+                            low: video[0].url,
+                        };
+                        if (video[1] && video[1].url) return_video_quality.medium = video[1].url;
+                        if (video[2] && video[2].url) return_video_quality.high = video[2].url;
+                        const return_data_picker = {
+                            type: 'picker',
+                            link: return_video_quality,
+                        };
+                        return return_data_picker;
+                    // Video quality selector disabled
+                    } else if (picker_enable === 'false') {
+                        video.sort(function (a, b) {
+                            return b.bitrate - a.bitrate;
+                        });
+                        const return_data_video = {
+                            type: media_type,
+                            link: video[0].url,
+                        };
+                        return return_data_video;
+                    }
+                }
+                // GIF
+                if (media_type === 'animated_gif') {
+                    const gif = media.video_info.variants.filter(variant => variant.content_type == 'video/mp4');
+                    const return_data_gif = {
+                        type: media_type,
+                        width: media.sizes.large.w,
+                        height: media.sizes.large.h,
+                        link: gif[0].url,
+                    };
+                    return return_data_gif;
+                }
+                // Photo
+                if (media_type === 'photo') {
+                    const media_link = media.media_url_https;
+                    const extension = media_link.match(/\.[a-z]+$/gi).shift();
+                    const media_link_no_extension = media_link.replace(extension, '');
+                    const file_extension = extension.replace('.', '');
+                    const full_res = 'orig';
+                    const final_media_link = media_link_no_extension + '?format=' + file_extension + '&name=' + full_res;
+                    const return_data_image = {
+                        type: media_type,
+                        link: final_media_link,
+                    };
+                    return return_data_image;
+                }
+            })
+            .filter(item => !!item);
     }
-    // Success, tweet has media
-    return tweet.extended_entities.media
-        .map(media => {
-            const media_type = media.type
-            // If media in the tweet is a video do this
-            if (media_type === 'video') {
-                const video = media.video_info.variants.filter(variant => variant.bitrate !== undefined)
-                video.sort(function (a, b) {
-                    return a.bitrate - b.bitrate;
-                });
-                const return_video_quality = {
-                    low: video[0].url,
-                }
-                if (video[1] && video[1].url) return_video_quality.medium = video[1].url
-                if (video[2] && video[2].url) return_video_quality.high = video[2].url
-                const return_data_video = {
-                    type: media_type,
-                    link: return_video_quality,
-                }
-                return return_data_video;
-            }
-            // If media in the tweet is a gif do this
-            if (media_type === 'animated_gif') {
-                const gif = media.video_info.variants.filter(variant => variant.content_type == 'video/mp4')
-                const return_data_gif = {
-                    type: media_type,
-                    width: media.sizes.large.w,
-                    height: media.sizes.large.h,
-                    link: gif[0].url,
-                }
-                return return_data_gif;
-            }
-            // If media in the tweet is a photo or photos do this
-            if (media_type === 'photo') {
-                const media_link = media.media_url_https;
-                const extension = media_link.match(/\.[a-z]+$/gi).shift();
-                const media_link_no_extension = media_link.replace(extension, '');
-                const file_extension = extension.replace('.', '');
-                const full_res = '4096x4096';
-                const final_media_link = `${media_link_no_extension}?format=${file_extension}&name=${full_res}`;
-                const return_data_image = {
-                    type: media_type,
-                    link: final_media_link,
-                }
-                return return_data_image;
-            }
-        })
-        .filter(item => !!item);
-}
-// Convert result to json
-const media_links = getmedialinks(tweet);
-const media_links_data = {
-    media: media_links
+    const media_links = getmedialinks(tweet);
+    const media_links_data = {
+        media: media_links,
+    };
+    return new Response(JSON.stringify(media_links_data), { status: 200 });
 };
-const media_links_json = JSON.stringify(media_links_data);
-document.write(media_links_json);
+async function handleMedia(request) {
+    return await twitterMedia(request);
+}
