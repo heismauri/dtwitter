0abe44bc1b65a47717ef8c37a318d26ff0f3ad70
Mauricio Villa <vbamauricio@gmail.com>
2021-08-03 13:57:53 -0400

    Added original/base script

---
+++ go-fetch-script.js
@@ -0,0 +1,49 @@
+var tweet = tweet
+    var media_urls = []
+
+    function getMediaUrls(tweet) {
+
+      if (!tweet.extended_entities.media) return
+
+
+      var murls = tweet.extended_entities.media.map( media => {
+
+        var media_type = media.type=='animated_gif'?'video':media.type
+
+        switch (media_type) {
+          case 'video':
+
+            var variants = media.video_info.variants.filter( variant => variant.content_type=='video/mp4')
+
+            return variants[0].url
+            break;
+          case 'photo':
+            var sizes = ['large','medium','small','thumb']
+            var avail = sizes.filter( size => {
+              return !!media.sizes[size]
+            })
+            var media_url = media.media_url_https
+            var extension = media_url.match(/\.[a-z]+$/gi)
+            media_url = media_url.replace(extension,'')
+            extension = (''+extension).replace('.','')
+
+            var max_size = avail[0]
+            max_size = '4096x4096' // hack for 4k
+            media_url = media_url + '?format=' + extension + '&name=' + max_size
+
+            return media_url
+            break;
+          default:
+            return null
+        }
+
+      })
+      murls = murls.filter( item => !!item)
+      return murls;
+    }
+
+    media_urls = getMediaUrls(tweet)
+
+    document.write(JSON.stringify({
+      urls:media_urls
+    }))
