ebe15482482874480102152786c45e4eae361455
Mauricio Villa <vbamauricio@gmail.com>
2022-09-29 10:44:04 -0300

    Refactored code for better error handling

--- worker.js
+++ worker.js
@@ -1,12 +1,51 @@
 const shortcutId = '6166';
 const shortcutName = 'DTwitter';
-const supportedVersions = ['3.0.7', '3.0.8'];
-const htmlResponse = ''; // index.html
+const supportedVersions = ['3.0.8'];
+const landingPage = '';// index.html
+
+// return Response with its corresponding Content-Type
+const addHeaders = (body) => {
+  return new Response(JSON.stringify(body), {
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    }
+  });
+};
+
+// Check params
+const paramsBuilder = (form) => {
+  const cleanParams = {};
+  // Check if installed version is the lastest one
+  const installedVersion = form.get('version') && form.get('version');
+  if (!installedVersion || !supportedVersions.includes(installedVersion)) {
+    cleanParams.message = `Download the latest update on https://routinehub.co/shortcut/${shortcutId}/`;
+    return cleanParams;
+  }
+  cleanParams.version = installedVersion;
+  // Check for valid URL
+  const tweetURL = form.get('url') && form.get('url').split('?')[0];
+  if (!tweetURL || !tweetURL.includes('twitter.com')) {
+    cleanParams.message = `URL not supported by ${shortcutName}`;
+    return cleanParams;
+  }
+  // Check for valid tweet ID
+  const tweetID = tweetURL.split('?')[0].split('/')[5];
+  if (!tweetID || !/\d{8,}/.test(tweetID)) {
+    cleanParams.message = 'The Tweet URL contains invalid parameters';
+    return cleanParams;
+  }
+  cleanParams.id = tweetID;
+  const selector = form.get('selector') && JSON.parse(form.get('selector')).selector;
+  if (selector) {
+    cleanParams.selector = selector;
+  }
+  return cleanParams;
+};
 
 // Build the response
-const jsonBuilder = (twitterJSON, selectorEnable) => {
+const jsonBuilder = (json, isSelectorEnabled) => {
   const dtwitterJSON = {
-    media: twitterJSON.extended_entities.media.map((media) => {
+    media: json.extended_entities.media.map((media) => {
       let mediaTweet;
       const mediaType = media.type;
       // Video & GIFs
@@ -14,7 +53,7 @@ const jsonBuilder = (twitterJSON, selectorEnable) => {
         const video = media.video_info.variants.filter((variant) => variant.bitrate !== undefined);
         video.sort((a, b) => a.bitrate - b.bitrate);
         // Quality selector
-        if (selectorEnable && mediaType === 'video') {
+        if (isSelectorEnabled && mediaType === 'video') {
           const videoQuality = {
             low: video[0].url,
           };
@@ -23,13 +62,13 @@ const jsonBuilder = (twitterJSON, selectorEnable) => {
           if (video[2] && video[2].url) videoQuality.high = video[2].url;
           mediaTweet = {
             type: 'selector',
-            link: videoQuality,
+            link: videoQuality
           };
           // High resolution video
         } else {
           mediaTweet = {
             type: mediaType,
-            link: video[video.length - 1].url,
+            link: video[video.length - 1].url
           };
           // Only return sizes for GIFs
           if (mediaType === 'animated_gif') {
@@ -46,7 +85,7 @@ const jsonBuilder = (twitterJSON, selectorEnable) => {
         const finalPhotoLink = `${photoLink}?format=${photoExtension}&name=orig`;
         mediaTweet = {
           type: mediaType,
-          link: finalPhotoLink,
+          link: finalPhotoLink
         };
       }
       return mediaTweet;
@@ -56,75 +95,47 @@ const jsonBuilder = (twitterJSON, selectorEnable) => {
   return dtwitterJSON;
 };
 
-// return Response with its corresponding Content-Type
-const addHeaders = (body) => {
-  return new Response(JSON.stringify(body), {
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    }
-  });
-};
-
 // Call the Twitter API 1.1
 const handleRequest = async (request) => {
-  let dtwitterResponse;
-  const dtwitterForm = await request.formData();
-  // Check if installed version is the lastest one
-  const installedVersion = dtwitterForm.get('version');
-  if (!(supportedVersions.includes(installedVersion))) {
-    dtwitterResponse = {
-      error: `Download the latest update on https://routinehub.co/shortcut/${shortcutId}/`
-    };
+  if (request.method === 'POST') {
+    let dtwitterResponse;
+    const params = paramsBuilder(await request.formData());
+    if (params.message) {
+      dtwitterResponse = {
+        error: params.message
+      };
+      return addHeaders(dtwitterResponse);
+    }
+    const twitterAPI = await fetch(`https://api.twitter.com/1.1/statuses/show.json?tweet_mode=extended&id=${params.id}`, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`
+      },
+    })
+      .then((response) => response.json());
+    // Check if the API gave them for any errors
+    if (twitterAPI.errors !== undefined) {
+      dtwitterResponse = {
+        error: twitterAPI.errors[0].message.replace('.', '')
+      };
+      // Check if the tweet has media on it
+    } else if (twitterAPI.extended_entities === undefined) {
+      dtwitterResponse = {
+        error: 'Media not found for inputted URL'
+      };
+    } else {
+      // Success
+      dtwitterResponse = jsonBuilder(twitterAPI, params.selector);
+    }
     return addHeaders(dtwitterResponse);
   }
-  const tweetURL = dtwitterForm.get('url');
-  if (!tweetURL.includes('twitter.com')) {
-    dtwitterResponse = {
-      error: `URL not supported by ${shortcutName}`
-    };
-    return addHeaders(dtwitterResponse);
-  }
-  const tweetId = tweetURL.split('?')[0].split('/')[5];
-  const twitterAPI = await fetch(`https://api.twitter.com/1.1/statuses/show.json?tweet_mode=extended&id=${tweetId}`, {
+  return new Response(landingPage, {
     headers: {
-      Authorization: `Bearer ${TOKEN}`,
+      'Content-Type': 'text/html; charset=UTF-8'
     },
-  })
-    .then((response) => response.json());
-  // Check if the API gave them for any errors
-  if (twitterAPI.errors !== undefined) {
-    dtwitterResponse = {
-      error: twitterAPI.errors[0].message.replace('.', ''),
-    };
-  // Check if the tweet has media on it
-  } else if (twitterAPI.extended_entities === undefined) {
-    dtwitterResponse = {
-      error: 'Media not found for inputted URL',
-    };
-  // Success
-  } else {
-    const selector = dtwitterForm.get('selector') && JSON.parse(dtwitterForm.get('selector')).selector;
-    dtwitterResponse = jsonBuilder(twitterAPI, selector);
-  }
-  return addHeaders(dtwitterResponse);
+  });
 };
 
 // Workers event listener
 addEventListener('fetch', (event) => {
-  const { request } = event;
-  if (request.method === 'POST') {
-    event.respondWith(
-      handleRequest(event.request).catch(
-        (error) => new Response(error.stack, { status: 500 })
-      )
-    );
-  } else {
-    event.respondWith(
-      new Response(htmlResponse, {
-        headers: {
-          'Content-Type': 'text/html; charset=UTF-8'
-        },
-      })
-    );
-  }
+  event.respondWith(handleRequest(event.request));
 });
