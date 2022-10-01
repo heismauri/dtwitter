1f544a7b2534891727deb9dbe935a0492348cea6
Mauricio Villa <vbamauricio@gmail.com>
2022-10-01 13:52:41 -0300

    Use undici instead of fetch and formdata

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,4 @@
-import fetch from 'node-fetch';
-import FormData from 'form-data';
+import { fetch, FormData } from 'undici';
 
 const LatestVersion = '3.0.8';
 
@@ -8,13 +7,13 @@ test('Can download videos', async () => {
   form.append('version', LatestVersion);
   form.append('url', 'https://twitter.com/KDA_MUSIC/status/1333078270640795649');
   form.append('selector', '{"selector":false}');
-  const requestAPI = await fetch('http://localhost:8787', {
+  const dtwitterAPI = await fetch('http://localhost:8787', {
     method: 'POST',
     body: form
   })
     .then((response) => response.json());
-  expect(requestAPI.media[0].type).toBe('video');
-  expect(requestAPI.media[0].link).toMatch(/video.twimg.com/);
+  expect(dtwitterAPI.media[0].type).toBe('video');
+  expect(dtwitterAPI.media[0].link).toMatch(/video.twimg.com/);
 });
 
 test('Can download videos with quality selector', async () => {
@@ -22,39 +21,39 @@ test('Can download videos with quality selector', async () => {
   form.append('version', LatestVersion);
   form.append('url', 'https://twitter.com/KDA_MUSIC/status/1333078270640795649');
   form.append('selector', '{"selector":true}');
-  const requestAPI = await fetch('http://localhost:8787', {
+  const dtwitterAPI = await fetch('http://localhost:8787', {
     method: 'POST',
     body: form
   })
     .then((response) => response.json());
-  expect(requestAPI.media[0].type).toBe('selector');
-  expect(requestAPI.media[0].link.high).toMatch(/video.twimg.com/);
+  expect(dtwitterAPI.media[0].type).toBe('selector');
+  expect(dtwitterAPI.media[0].link.high).toMatch(/video.twimg.com/);
 });
 
 test('Can download GIFs', async () => {
   const form = new FormData();
   form.append('version', LatestVersion);
   form.append('url', 'https://twitter.com/yeji_gif/status/1568177615865020416');
-  const requestAPI = await fetch('http://localhost:8787', {
+  const dtwitterAPI = await fetch('http://localhost:8787', {
     method: 'POST',
     body: form
   })
     .then((response) => response.json());
-  expect(requestAPI.media[0].type).toBe('animated_gif');
-  expect(requestAPI.media[0].link).toMatch(/video.twimg.com/);
-  expect(typeof requestAPI.media[0].width).toBe('number');
-  expect(typeof requestAPI.media[0].height).toBe('number');
+  expect(dtwitterAPI.media[0].type).toBe('animated_gif');
+  expect(dtwitterAPI.media[0].link).toMatch(/video.twimg.com/);
+  expect(typeof dtwitterAPI.media[0].width).toBe('number');
+  expect(typeof dtwitterAPI.media[0].height).toBe('number');
 });
 
 test('Can download photos', async () => {
   const form = new FormData();
   form.append('version', LatestVersion);
   form.append('url', 'https://twitter.com/KDA_MUSIC/status/1331266329819623426');
-  const requestAPI = await fetch('http://localhost:8787', {
+  const dtwitterAPI = await fetch('http://localhost:8787', {
     method: 'POST',
     body: form
   })
     .then((response) => response.json());
-  expect(requestAPI.media[0].type).toBe('photo');
-  expect(requestAPI.media[0].link).toMatch(/pbs.twimg.com/);
+  expect(dtwitterAPI.media[0].type).toBe('photo');
+  expect(dtwitterAPI.media[0].link).toMatch(/pbs.twimg.com/);
 });
