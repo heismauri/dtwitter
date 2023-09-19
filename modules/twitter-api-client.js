51e977ae510b698a10739dd608aa366c27d9ed21
Mauricio Villa <vbamauricio@gmail.com>
2023-09-19 20:32:11 -0300

    Sorted keys alphabetically

--- modules/twitter-api-client.js
+++ modules/twitter-api-client.js
@@ -2,24 +2,24 @@ import { getGuestToken, twitterAPIGuestClient } from './get-token';
 
 const paramsFeatures = {
   'creator_subscriptions_tweet_preview_api_enabled': true,
-  'tweetypie_unmention_optimization_enabled': true,
-  'responsive_web_edit_tweet_api_enabled': true,
-  'graphql_is_translatable_rweb_tweet_is_translatable_enabled': true,
-  'view_counts_everywhere_api_enabled': true,
-  'longform_notetweets_consumption_enabled': true,
-  'responsive_web_twitter_article_tweet_consumption_enabled': false,
-  'tweet_awards_web_tipping_enabled': false,
   'freedom_of_speech_not_reach_fetch_enabled': true,
-  'standardized_nudges_misinfo': true,
-  'tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled': true,
-  'longform_notetweets_rich_text_read_enabled': true,
+  'graphql_is_translatable_rweb_tweet_is_translatable_enabled': true,
+  'longform_notetweets_consumption_enabled': true,
   'longform_notetweets_inline_media_enabled': true,
+  'longform_notetweets_rich_text_read_enabled': true,
+  'responsive_web_edit_tweet_api_enabled': true,
+  'responsive_web_enhance_cards_enabled': false,
   'responsive_web_graphql_exclude_directive_enabled': true,
-  'verified_phone_label_enabled': false,
-  'responsive_web_media_download_video_enabled': false,
   'responsive_web_graphql_skip_user_profile_image_extensions_enabled': false,
   'responsive_web_graphql_timeline_navigation_enabled': true,
-  'responsive_web_enhance_cards_enabled': false
+  'responsive_web_media_download_video_enabled': false,
+  'responsive_web_twitter_article_tweet_consumption_enabled': false,
+  'standardized_nudges_misinfo': true,
+  'tweet_awards_web_tipping_enabled': false,
+  'tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled': true,
+  'tweetypie_unmention_optimization_enabled': true,
+  'verified_phone_label_enabled': false,
+  'view_counts_everywhere_api_enabled': true
 };
 
 const paramsFieldToggles = {
@@ -29,19 +29,19 @@ const paramsFieldToggles = {
 const twitterHeaders = async (env) => {
   const guestToken = await getGuestToken(env);
   return {
+    'Authorization': `Bearer ${env.TOKEN}`,
     'Origin': 'https://twitter.com',
     'Referer': 'https://twitter.com/',
-    'x-twitter-active-user': 'yes',
     'x-guest-token': String(guestToken),
-    'Authorization': `Bearer ${env.TOKEN}`
+    'x-twitter-active-user': 'yes'
   };
 };
 
 const paramsVariables = (id) => {
   return {
+    'includePromotedContent': false,
     'tweetId': String(id),
     'withCommunity': false,
-    'includePromotedContent': false,
     'withVoice': false
   };
 };
@@ -51,9 +51,9 @@ const twitterAPIClient = async (id, env, firstTry = true) => {
     const twitterGraphqlHeaders = await twitterHeaders(env);
     const twitterGraphql = new URL('https://api.twitter.com/graphql/3HC_X_wzxnMmUBRIn3MWpQ/TweetResultByRestId');
     twitterGraphql.search = new URLSearchParams({
-      'variables': JSON.stringify(paramsVariables(id)),
       'features': JSON.stringify(paramsFeatures),
-      'fieldToggles': JSON.stringify(paramsFieldToggles)
+      'fieldToggles': JSON.stringify(paramsFieldToggles),
+      'variables': JSON.stringify(paramsVariables(id))
     });
     const twitterGraphqlResponse = await fetch(twitterGraphql, {
       headers: twitterGraphqlHeaders
