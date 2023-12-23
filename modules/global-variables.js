70ba16dbc55909d9d102685ba1f5d25d20637388
Mauricio Villa <vbamauricio@gmail.com>
2023-12-22 23:01:40 -0300

    Updated versions

--- modules/global-variables.js
+++ modules/global-variables.js
@@ -1,7 +1,7 @@
 const shortcutId = '6166';
 const shortcutName = 'DTwitter';
 const shortcutURL = `https://routinehub.co/shortcut/${shortcutId}/`;
-const supportedVersions = ['4.1.1', '4.2.0'];
+const supportedVersions = ['4.1.1', '4.2.1'];
 const latestVersion = supportedVersions[supportedVersions.length - 1];
 
 export {
